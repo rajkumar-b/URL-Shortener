5079266bac75e9cdb493c58a27f832fc05eedc78
rajkumarneville <rajkumarneville@gmail.com>
2019-08-13 16:28:46 +0530

    Create a basic index page to get and display url

---
+++ WebContent/WEB-INF/view/index.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>URL Shortener - Demo Site</title>
+</head>
+<body>
+	<form action="shortenURL" method="post">
+		<input type="url" name="inputUrlReal" placeholder="Enter the URL to Shorten"/>
+		<input type="submit" value="Get Shortened URL!"/>
+		<input type="text" name="shortenedUrl" value="${shortenedUrl}"/>
+	</form>
+	
+	<h4>OR</h4>
+	
+	<form action="expandURL" method="post">
+		<input type="url" name="inputUrlShort" placeholder="Enter the URL to Expand"/>
+		<input type="submit" value="Get Real URL!"/>
+		<input type="text" name="expandedUrl" value="${expandedUrl}"/>
+	</form>
+	
+</body>
+</html>
--- src/com/datasigns/demoapp/controller/MainController.java
+++ src/com/datasigns/demoapp/controller/MainController.java
@@ -1,13 +1,34 @@
 package com.datasigns.demoapp.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
 	@RequestMapping("/")
-	public String getIndex() {
+	public String getIndex(Model model) {
+		model.addAttribute("shortenedUrl", "");
+		model.addAttribute("expandedUrl", "");
+		return "index";
+	}
+	
+	@RequestMapping("/shortenURL")
+	public String shortenURL(HttpServletRequest request, Model model) {
+		String shortenedUrl = "dummyValue";
+		model.addAttribute("shortenedUrl", shortenedUrl);
+		model.addAttribute("expandedUrl", "");
+		return "index";
+	}
+	
+	@RequestMapping("/expandURL")
+	public String expandURL(HttpServletRequest request, Model model) {
+		String expandedUrl = "dummyValue";
+		model.addAttribute("shortenedUrl", "");
+		model.addAttribute("expandedUrl", expandedUrl);
 		return "index";
 	}
 }
